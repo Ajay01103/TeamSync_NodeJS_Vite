addb20c62c92c33671ca00c774e930f6a8143f38
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2025-04-26 18:23:41 +0530

    backend done & fontend initialized

---
+++ frontend/src/lib/axios-client.ts
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+const baseURL = import.meta.env.VITE_API_BASE_URL
+
+const options = {
+  baseURL,
+  withCredentials: true,
+  timeout: 10000,
+}
+
+const API = axios.create(options)
+
+API.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error) => {
+    if (error.response) {
+      const { data, status } = error.response
+      if (data === "Unauthorized" && status === 401) {
+        window.location.href = "/"
+      }
+      return Promise.reject({
+        ...data,
+      })
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default API
---
+++ frontend/src/routes/auth.route.tsx
@@ -0,0 +1,20 @@
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton"
+import useAuth from "@/hooks/api/use-auth"
+import { Navigate, Outlet } from "react-router-dom"
+
+const AuthRoute = () => {
+  const { data: authData, isLoading } = useAuth()
+  const user = authData?.user
+
+  if (isLoading) {
+    return <DashboardSkeleton />
+  }
+
+  if (!user) {
+    return <Outlet />
+  }
+
+  return <Navigate to={`/workspace/${user.currentWorkspace?._id}`} />
+}
+
+export default AuthRoute
